6af727520f05065d3fab62037a066304447dda1e
Leslie <leslie.lianggangwei@foxmail.com>
2021-04-18 22:33:19 +0800

    完成 parcel 安装，并创建 button.vue 单组件文件。 将 app.js （vue 主文件）独立，index.html 添加依赖。 package.json 添加任务脚本（dev、build），"alias" gitignore 添加 dist、.cache 文件

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from './component/button'
+
+Vue.component('g-button', Button)
+
+new Vue({
+    el: '#app'
+})
---
+++ src/component/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="g-button">button</button>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px  solid var(--border-color);
+    background:var(--button-bg);
+    outline: none;
+    &:hover {
+      color: var(--border-color);
+      border-color: var(--border-color-hover);
+      background: var(--button-hover-bg);
+    }
+    &:active {
+      color:var(--button-bg);
+      background: var(--button-active-bg);
+    }
+  }
+</style>
