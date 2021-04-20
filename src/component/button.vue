b0c725e1fced5ed5b06c22dbeaf85f2f6d4c5d2a
Leslie <leslie.lianggangwei@foxmail.com>
2021-04-20 10:05:30 +0800

    增加 iconfont ，icon 添加 “Position”属性

--- src/component/button.vue
+++ src/component/button.vue
@@ -1,28 +1,62 @@
 <template>
-  <button class="g-button">button</button>
+  <button class="g-button" :class=`icon-${iconPosition}`>
+    <svg v-if='icon' class="icon" aria-hidden="true">
+      <use :xlink:href=`#icon-${icon}`></use>
+    </svg>
+    <slot></slot>
+  </button>
 </template>
 
 <script>
-  export default {}
+export default {
+  props: ['icon', 'iconPosition']
+}
 </script>
 
 <style lang="scss">
   .g-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
     border-radius: var(--border-radius);
-    border: 1px  solid var(--border-color);
-    background:var(--button-bg);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
     outline: none;
+
     &:hover {
-      color: var(--border-color);
+      color: var(--button-bg);
       border-color: var(--border-color-hover);
       background: var(--button-hover-bg);
+
+      .icon {
+        fill: var(--button-bg);
+      }
     }
+
     &:active {
-      color:var(--button-bg);
+      color: var(--border-color);
       background: var(--button-active-bg);
+
+      .icon {
+        fill: var(--border-color);
+      }
+    }
+
+    &.icon-right {
+      .icon {
+        order: 2;
+      }
+    }
+
+    .icon {
+      width: 1em;
+      height: 1em;
+      margin-right: .1em;
+      fill: var(--font-size)
     }
   }
 </style>
---
+++ src/component/icon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg v-if='icon' class="icon" aria-hidden="true">
+    <use :xlink:href=`#icon-${icon}`></use>
+  </svg>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
