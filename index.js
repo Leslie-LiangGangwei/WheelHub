58269ceca7d065a732fd99ba451d90d08c4b7723
Leslie <leslie.lianggangwei@foxmail.com>
2021-04-28 17:04:47 +0800

    update iconfont.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Button from './src/component/button'
 import ButtonGroup from './src/component/button-group'
 import Icon from './src/component/icon'
+import '../icon/iconfont'
 
 export {Button, ButtonGroup, Icon}
