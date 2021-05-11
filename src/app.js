8671bd33d53aebd8cc531d8c7d17a53ddfb7ae94
Leslie <leslie.lianggangwei@foxmail.com>
2021-05-11 17:17:22 +0800

    add row&col component

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './component/button'
 import Icon from './component/icon'
 import ButtonGroup from './component/button-group'
 import Input from './component/input'
+import Row from './component/row'
+import Col from './component/col'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 new Vue({
     el: '#app',
---
+++ src/component/col.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Col',
+  props: {
+    span: {
+      type: [String, Number]
+    },
+    offset: {
+      type: [String, Number]
+    },
+    gutter: {
+      type: [String, Number]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+  width: 50%;
+  height: 50px;
+  background-color: gray;
+  border: 1px solid blue;
+
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: percentage($n / 24);
+    }
+  }
+}
+</style>
