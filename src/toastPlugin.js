22ca69ab4033b70cc14ec38c8542c5a69e9e97f7
Leslie <leslie.lianggangwei@foxmail.com>
2021-05-23 15:37:03 +0800

    重构代码（实现当前只会有一个 toast）

--- src/toastPlugin.js
+++ src/toastPlugin.js
@@ -1,15 +1,23 @@
 import Toast from '/src/component/toast'
 
+let currentToast
+
 export default {
     install(Vue, options) {
-        Vue.prototype.$toast = function (message, toastProps) {
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor({
-                propsData: toastProps
-            })
-            toast.$slots.default = message
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+        Vue.prototype.$toast = function (message, propsData) {
+            if (currentToast) {
+                currentToast.remove()
+            }
+            currentToast = createToast({Vue, message, propsData})
         }
     }
+}
+
+function createToast({Vue, message, propsData}) {
+    let Constructor = Vue.extend(Toast)
+    let toast = new Constructor({propsData})
+    toast.$slots.default = message
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
 }
