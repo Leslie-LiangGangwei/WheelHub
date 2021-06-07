82469f47bd9c70c599110d05fb3c33ee5067ba31
Leslie <leslie.lianggangwei@foxmail.com>
2021-06-07 09:15:37 +0800

    'props 接受 hover 事件' 测试用例未实现

--- test/popover.test.js
+++ test/popover.test.js
@@ -24,7 +24,30 @@ describe('Popover', () => {
         setTimeout(() => {
             const popoverElement = document.body.querySelector('.content-wrapper')
             expect(popoverElement.classList.contains('position-top')).to.be.true
+            div.remove()
+            popoverElement.remove()
             done()
         }, 200)
     })
+    // it(props 接受 hover 事件, (done) => {
+    //     Vue.component('g-popover', Popover)
+    //     const div = document.createElement('div')
+    //     document.body.appendChild(div)
+    //     div.innerHTML = `
+    //     <g-popover position="top" trigger="hover">
+    //         <template slot="content">这是 popover 内容</template>
+    //         <button>点我</button>
+    //     </g-popover>
+    //     `
+    //     const vm = new Vue({el: div})
+    //     const popoverButton = vm.$el.querySelector('button')
+    //     popoverButton.onmouseover()
+    //     setTimeout(() => {
+    //         const popoverElement = document.body.querySelector('.content-wrapper')
+    //         expect(popoverElement).to.be.exist
+    //         div.remove()
+    //         popoverElement.remove()
+    //         done()
+    //     }, 200)
+    // })
 })
