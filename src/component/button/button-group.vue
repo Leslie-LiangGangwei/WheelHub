84a72899d9918372eec807526ab936c402d2134e
Leslie <leslie.lianggangwei@foxmail.com>
2021-06-15 14:51:39 +0800

    同步 scss

--- src/component/button/button-group.vue
+++ src/component/button/button-group.vue
@@ -19,6 +19,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$button-height: 32px;
+$font-size: 14px;
+$button-bg: #fff;
+$button-hover-bg: #666;
+$border-color-active: #eee;
+$button-active-bg: #eee;
+$border-radius: 4px;
+$color: #1C1C1C;
+$border-color: #999;
 .g-button-group {
   display: inline-flex;
   vertical-align: middle;
@@ -28,23 +37,23 @@ export default {
       margin-left: -1px;
     }
     &:first-child {
-      border-top-left-radius: var(--border-radius);
-      border-bottom-left-radius: var(--border-radius);
+      border-top-left-radius: $border-radius;
+      border-bottom-left-radius: $border-radius;
     }
     &:last-child {
-      border-top-right-radius: var(--border-radius);
-      border-bottom-right-radius: var(--border-radius);
+      border-top-right-radius: $border-radius;
+      border-bottom-right-radius: $border-radius;
     }
     &:hover {
       z-index: 1;
-      border-color: var(--button-hover-bg);
+      border-color: $button-hover-bg;
     }
     &:active {
-      color: var(--border-color);
-      background: var(--button-active-bg);
-      border-color: var(--font-size);
+      color: $border-color;
+      background: $button-active-bg;
+      border-color: $font-size;
       .g-icon {
-        fill: var(--border-color);
+        fill: $border-color;
       }
     }
   }
