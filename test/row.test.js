8c0fbafe2788c34e795a360ab4360713f46614d2
Leslie <leslie.lianggangwei@foxmail.com>
2021-06-16 10:46:53 +0800

    row 添加 gutter: {iphone: 8} 测试用例

--- test/row.test.js
+++ test/row.test.js
@@ -1,7 +1,6 @@
 const expect = chai.expect;
 import Vue from 'vue'
 import Row from '../src/component/grid/row'
-import Col from '../src/component/grid/col'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
@@ -10,30 +9,19 @@ describe('Row', () => {
     it('存在.', () => {
         expect(Row).to.be.ok
     })
-    it('测试接收 props', (done) => {
-        Vue.component('g-row', Row)
-        Vue.component('g-col', Col)
+    it('接受 gutter 属性', () => {
         const div = document.createElement('div')
         document.body.appendChild(div)
-        div.innerHTML = `
-        <g-row gutter="16">
-            <g-col span="12"></g-col>
-            <g-col span="12"></g-col>
-        </g-row>
-        `
-        const vm = new Vue({el: div})
-        setTimeout(() => {
-            const row = vm.$el.querySelector('.row')
-            expect(getComputedStyle(row).marginLeft).to.equal('-8px')
-            expect(getComputedStyle(row).marginRight).to.equal('-8px')
-            const col = vm.$el.querySelector('.col')
-            expect(getComputedStyle(col).paddingLeft).to.equal('8px')
-            expect(getComputedStyle(col).paddingRight).to.equal('8px')
-            done()
-            div.remove()
-            vm.$el.remove()
-            vm.$destroy()
-        })
+        const Constructor = Vue.extend(Row)
+        let vm = new Constructor({
+            propsData: {
+                gutter: {iphone: 8}
+            }
+        }).$mount(div)
+        const rowElement = vm.$el
+        expect(rowElement.classList.contains('gutter-iphone-8')).to.equal(true)
+        div.remove()
+        vm.$destroy()
     })
     it('接受 justify 属性', () => {
         const div = document.createElement('div')
