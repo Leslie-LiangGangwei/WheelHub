709d6b2af5a21c12089923b8d47b14491435d141
Leslie <leslie.lianggangwei@foxmail.com>
2021-06-23 17:50:20 +0800

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Leslie-LiangGangwei/WheelHUb.git master:gh-pages
+git push -f git@github.com:Leslie-LiangGangwei/WheelHub.git master:gh-pages
 
 cd -
